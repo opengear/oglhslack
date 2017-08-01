24ebe009f946a2af405af7ddb7d20ba896742886
Thiago Melo <thiago.lc.melo@gmail.com>
2017-08-01 01:22:43 -0300

    some improvements in logging

--- python27/oglh_bot.py
+++ python27/oglh_bot.py
@@ -1,34 +1,29 @@
 #!/usr/bin/env python
 
-import os, textwrap, re, time, multiprocessing, threading, logging
+import os, signal, textwrap, re, time, multiprocessing, threading, logging
 from oglh_client import LighthouseApi
 from slackclient import SlackClient
 from functools import wraps
 
-def report_failure(f):
-    def wrapper(*args):
-        try:
-            result = f(*args)
-        except Exception as error:
-            args[0]._warning_message(str(error))
-    return wrapper
-
-class OgLhSimpleClient:
-    """
-    Provides a minimum user of the LighthouseApi with methods that return
-    parsed responses rather than the actual JSON responses
-    """
-
 class OgLhSlackBot:
     """
+    Provides a minimum use of the LighthouseApi with methods that return
+    parsed responses rather than the actual JSON responses so that
 
+    Although the list() method keeps listening for request from the Slack and
+    makes possible to access all other features throught there, most of them
+    can be called directly from:
+
+    >>> from oglh_bot import OgLhSlackBot
+    >>> slack_bot = OgLhSlackBot()
     """
 
     def __init__(self):
         self.bot_name = os.environ.get('SLACK_BOT_NAME')
         self.default_channel = os.environ.get('SLACK_BOT_DEFAULT_CHANNEL')
         self.default_log_channel = os.environ.get('SLACK_BOT_DEFAULT_LOG_CHANNEL')
-        self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+        self.slack_token = os.environ.get('SLACK_BOT_TOKEN')
+        self.slack_client = SlackClient(self.slack_token)
 
         self.lh_api = LighthouseApi()
         self.lh_client = self.lh_api.get_client()
@@ -57,90 +52,8 @@ class OgLhSlackBot:
         self.pending_name_ids = {}
         _, _ = self.get_pending()
 
-    def get_ports(self, label):
-        body = self.lh_client.nodes.list({ 'port:label': label })
-        return [port for node in body['nodes'] for port in node['ports'] if port['label'].lower() == label]
-
-    def get_pending(self):
-        body = self.lh_client.nodes.list({ 'config:status' : 'Registered' })
-        name_ids = { node['name']: node['id'] for node in body['nodes'] if node['approved'] == 0 }
-        new_pending = (set(name_ids) > set(self.pending_name_ids))
-        self.pending_name_ids = name_ids
-        return sorted(name_ids, key=lambda k: k.lower()), new_pending
-
-    def get_enrolled(self):
-        body = self.lh_client.nodes.list({ 'config:status' : 'Enrolled' })
-        return sorted([node['name'] for node in body['nodes']], key=unicode.lower)
-
-    def get_port_labels(self, node_name):
-        if node_name:
-            body = self.lh_client.nodes.list({ 'config:name' : node_name })
-        else:
-            body = self.lh_client.nodes.list()
-            labels = [port['label'] for node in body['nodes'] for port \
-                in node['ports'] if port['mode'] == 'consoleServer']
-        return sorted(labels, key=unicode.lower)
-
-    def get_summary(self):
-        body = self.lh_client.stats.nodes.connection_summary.get()
-        for conn in body['connectionSummary']:
-            if conn['status'] == 'connected':
-                connected = int(conn['count'])
-            elif conn['status'] == 'pending':
-                pending = int(conn['count'])
-                continue
-            elif conn['status'] == 'disconnected':
-                disconnected = int(conn['count'])
-        return connected, pending, disconnected
-
-    def _delete_node(self, node):
-        try:
-            result = self.lh_client.nodes.delete(id=node['id'])
-            return True
-        except Exception as e:
-            self._logging(str(e), level=logging.ERROR)
-        return None
-
-    def delete_nodes(self, node_names):
-        body = self.lh_client.nodes.list()
-        deleted_names = []
-        for node in body['nodes']:
-            if node['name'] in node_names:
-                if self._delete_node(node):
-                    deleted_names.append(node['name'])
-        return deleted_names
-
-    def _approve_node(self, node):
-        approved_node = {
-            'node': {
-                'name': node['name'],
-                'mac_address': '',
-                'description': '',
-                'approved': 1,
-                'tags': node['tag_list']['tags']
-            }
-        }
-        try:
-            result = self.lh_client.nodes.update(data=approved_node, id=node['id'])
-        except Exception as e:
-            #print e
-            self._logging(str(e), level=logging.ERROR)
-            return None
-        return True
-
-    def approve_nodes(self, node_names):
-        body = self.lh_client.nodes.list({ 'config:status' : 'Registered' })
-        approved_names = []
-        for node in body['nodes']:
-            if node['name'] in node_names:
-                if self._approve_node(node):
-                    approved_names.append(node['name'])
-        return approved_names
-
-
-
-
-
+    # Slack Bot methods
+    ## Methods for dealing with Slack Bot
 
     def _get_bot_id(self):
         try:
@@ -199,32 +112,31 @@ class OgLhSlackBot:
     def _approve_nodes(self, str_names, *_):
         names = str_names.split(' ')
         approved_names = self.approve_nodes(names)
-        response = ''
+        response = []
         for name in names:
             if name in approved_names:
                 emoji = 'approved :thumbsup:'
             else:
                 emoji = 'failed :thumbsdown:'
-            response +=  name + ' ' + emoji + '\n'
-        return response
+            response.append(name + ' ' + emoji)
+        return self._format_list(response)
 
     def _delete_nodes(self, str_names, *_):
         names = str_names.split(' ')
         deleted_names = self.delete_nodes(names)
-        response = ''
+        response = []
         for name in names:
             if name in deleted_names:
                 emoji = 'unenrolled :boom:'
             else:
                 emoji = 'failed :thumbsdown:'
-            response +=  name + ' ' + emoji + '\n'
-        return response
+            response.append(name + ' ' + emoji)
+        return self._format_list(response)
 
     def _get_port_labels(self, node_name, *_):
         labels = self.get_port_labels(node_name)
-
         if labels:
-            response = '\n'.join(labels)
+            response = self._format_list(labels)
         else:
             response = 'Nothing to see here'
         return response
@@ -233,7 +145,7 @@ class OgLhSlackBot:
         enrolled_nodes = self.get_enrolled()
 
         if enrolled_nodes:
-            response = '\n'.join(enrolled_nodes)
+            response = self._format_list(enrolled_nodes)
         else:
             response = 'Nothing to see here'
         return response
@@ -246,7 +158,7 @@ class OgLhSlackBot:
 
         if pending_nodes:
             response = 'Nodes await your approval!\n'
-            response += '\n'.join(pending_nodes)
+            response += self._format_list(pending_nodes)
         else:
             response = 'Nothing pending, as you were'
         return response
@@ -288,6 +200,9 @@ class OgLhSlackBot:
                 return username
         return 'nobody'
 
+    def _format_list(self, raw_list):
+        return '\n' + '\n'.join(['> %d. %s' % (i + 1, e) for i, e in enumerate(raw_list)])
+
     def _show_help(self):
         return textwrap.dedent("""
             ```
@@ -317,7 +232,7 @@ class OgLhSlackBot:
     def _command(self, command, channel, user_id):
         try:
             self.semaphores.acquire()
-            print 'fired: ' + threading.currentThread().getName()
+
             response = ''
             username = self._get_slack_username(user_id)
 
@@ -339,14 +254,16 @@ class OgLhSlackBot:
 
             response = response + output
             self._logging('Responding: ' + response)
+
+            try:
+                self.slack_client.api_call('chat.postMessage', channel=channel, \
+                    text=response, as_user=True)
+            except:
+                raise RuntimeError('Slack post failed')
+        except Exception as e:
+            self._logging(str(e), level=logging.ERROR)
         finally:
             self.semaphores.release()
-
-        try:
-            self.slack_client.api_call('chat.postMessage', channel=channel, \
-                text=response, as_user=True)
-        except:
-            raise RuntimeError('Slack post failed')
 
     def _read(self, output_list):
         if output_list and len(output_list) > 0:
@@ -355,21 +272,6 @@ class OgLhSlackBot:
                     return output['text'].split(self.bot_at)[1].strip().lower(), output['channel'], output['user']
         return None, None, None
 
-    @report_failure
-    def listen(self):
-        workers = {'Worker'+str(i):False for i in range(self.poll_max_workers)}
-        while True:
-            try:
-                command, channel, user_id = self._read(self.slack_client.rtm_read())
-            except:
-                raise RuntimeError('Slack read failed')
-            if command and channel and user_id:
-                t = threading.Thread(name='', target=self._command, \
-                    args=(command, channel, user_id))
-                t.setDaemon(True)
-                t.start()
-            time.sleep(self.poll_interval)
-
     def _warning_message(self, message):
         warning_message = textwrap.dedent("""
             @""" + self.bot_name + """  went offline with error message:
@@ -377,18 +279,200 @@ class OgLhSlackBot:
             """ + message + """
             ```
             """)
-        self.slack_client = SlackClient(os.environ.get('SLACK_BOT_TOKEN'))
+        self.slack_client = SlackClient(self.slack_token)
         self.slack_client.api_call('chat.postMessage', \
             channel=self.default_channel, text=warning_message, as_user=True)
 
-    def _logging(self, message, level=logging.INFO):
-        if self.default_log_channel:
+    def _logging(self, message, level=logging.INFO, force_slack=False):
+        """
+        it will log to slack only if there is a specified slack channel for logs
+        or if the level is not a simple logging.INFO
+        """
+
+        if self.default_log_channel and self.slack_client \
+            and (self.default_log_channel != self.default_channel \
+            or level > logging.INFO or force_slack):
+            slack_message = message
+            if level > logging.INFO:
+                slack_message = textwrap.dedent("""
+                    @""" + self.bot_name + """  would like you to know:
+                    ```
+                    """ + message + """
+                    ```
+                    """)
             self.slack_client.api_call('chat.postMessage', \
-                channel=self.default_log_channel, text=message, as_user=True)
-        if level == logging.ERROR:
+                channel=self.default_log_channel, text=slack_message, as_user=True)
+
+        if level == logging.CRITICAL:
+            logging.critical(message)
+        elif level == logging.ERROR:
             logging.error(message)
+        elif level == logging.WARNING:
+            logging.warning(message)
         else:
             logging.info(message)
+
+    def listen(self):
+        try:
+            while True:
+                try:
+                    command, channel, user_id = self._read(self.slack_client.rtm_read())
+                except:
+                    raise RuntimeError('Slack read failed')
+
+                if command and channel and user_id:
+                    t = threading.Thread(target=self._command, \
+                        args=(command, channel, user_id))
+                    t.setDaemon(True)
+                    t.start()
+
+                time.sleep(self.poll_interval)
+        except KeyboardInterrupt:
+            self._logging('Slack bot was interrupt manually', level=logging.WARNING)
+            os.kill(os.getpid(), signal.SIGUSR1)
+        except Exception as error:
+            self._warning_message(str(error))
+
+    # Lighthouse API methods
+    ## the following methods are those used to interact with the Lighthouse API
+
+    def get_ports(self, label):
+        """
+        Return all ports along all nodes such that the port's label matches
+        the :label paramater. Usage:
+
+        >>> ports = slack_bot.get_ports('mySoughtLabel')
+        """
+        body = self.lh_client.nodes.list({ 'port:label': label })
+        return [port for node in body['nodes'] for port in node['ports'] \
+            if port['label'].lower() == label]
+
+    def get_pending(self):
+        """
+        Usage:
+
+        >>> pending_node_names, new_pending = slack_bot.get_pending()
+
+        @pending_node_names is a list of names of the pending nodes
+        @new_pending is True if there is some new pending node since the bot
+            was instantiated, and False otherwise
+        """
+        body = self.lh_client.nodes.list({ 'config:status' : 'Registered' })
+        name_ids = { node['name']: node['id'] for node in body['nodes'] \
+            if node['approved'] == 0 }
+        new_pending = (set(name_ids) > set(self.pending_name_ids))
+        self.pending_name_ids = name_ids
+        return sorted(name_ids, key=lambda k: k.lower()), new_pending
+
+    def get_enrolled(self):
+        """
+        Usage:
+
+        >>> enrolled_node_names = slack_bot.get_enrolled()
+
+        @enrolled_node_names is a list of the currently enrolled nodes
+        """
+        body = self.lh_client.nodes.list({ 'config:status' : 'Enrolled' })
+        return sorted([node['name'] for node in body['nodes']], key=unicode.lower)
+
+    def get_port_labels(self, node_name):
+        """
+        Usage:
+
+        >>> port_labels = slack_bot.get_port_labels('myNodeOfInterest')
+
+        @port_labels is a list of port labels for a given node, specified by
+            its name
+        """
+        if node_name:
+            body = self.lh_client.nodes.list({ 'config:name' : node_name })
+        else:
+            body = self.lh_client.nodes.list()
+            labels = [port['label'] for node in body['nodes'] for port \
+                in node['ports'] if port['mode'] == 'consoleServer']
+        return sorted(labels, key=unicode.lower)
+
+    def get_summary(self):
+        """
+        Usage:
+
+        >>> connected, pending, disconnected = slack_bot.get_summary()
+
+        @connected is the number of connected nodes
+        @pending is the number of pending nodes
+        @disconnected is the number of disconnected nodes
+        """
+        body = self.lh_client.stats.nodes.connection_summary.get()
+        for conn in body['connectionSummary']:
+            if conn['status'] == 'connected':
+                connected = int(conn['count'])
+            elif conn['status'] == 'pending':
+                pending = int(conn['count'])
+                continue
+            elif conn['status'] == 'disconnected':
+                disconnected = int(conn['count'])
+        return connected, pending, disconnected
+
+    def delete_nodes(self, node_names):
+        """
+        :node_names is a list of names of nodes to be deleted
+
+        Usage:
+
+        >>> nodes = ['myNodeName1', 'myNodeName2', 'myNodeName3']
+        >>> deleted_list = slack_bot.delete_nodes(nodes)
+        >>> print deleted_list
+
+        @deleted_list is a subset of :node_names with those which were deleted
+        """
+        body = self.lh_client.nodes.list()
+        deleted_names = []
+        for node in body['nodes']:
+            if node['name'] in node_names:
+                try:
+                    result = self.lh_client.nodes.delete(id=node['id'])
+                    if type(result) is dict and 'error' in result \
+                        and len(result['error']) > 0:
+                        raise RuntimeError(result['error'][0]['text'])
+                    deleted_names.append(node['name'])
+                except Exception as e:
+                    self._logging(str(e), level=logging.ERROR)
+        return deleted_names
+
+    def approve_nodes(self, node_names):
+        """
+        :node_names is a list of names of nodes to be approved
+
+        Usage:
+
+        >>> nodes = ['myNodeName1', 'myNodeName2', 'myNodeName3']
+        >>> approved_list = slack_bot.approve_nodes(nodes)
+        >>> print approved_list
+
+        @approved_list is a subset of :node_names with those which were approved
+        """
+        body = self.lh_client.nodes.list({ 'config:status' : 'Registered' })
+        approved_names = []
+        for node in body['nodes']:
+            if node['name'] in node_names:
+                try:
+                    approved_node = {
+                        'node': {
+                            'name': node['name'],
+                            'mac_address': '',
+                            'description': '',
+                            'approved': 1,
+                            'tags': node['tag_list']['tags']
+                        }
+                    }
+                    result = self.lh_client.nodes.update(data=approved_node, id=node['id'])
+                    if type(result) is dict and 'error' in result \
+                        and len(result['error']) > 0:
+                        raise RuntimeError(result['error'][0]['text'])
+                    approved_names.append(node['name'])
+                except Exception as e:
+                    self._logging(str(e), level=logging.ERROR)
+        return approved_names
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO, filename='oglh_slack_bot.log',
